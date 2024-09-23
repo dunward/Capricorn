c856063e5e66f1ce22360f24c4ab22a6a379e828
Elik Song <elik.song@icloud.com>
2024-09-23 21:09:09 +0900

    Add graph view drag drop skeleton

--- Scripts/Editor/CapricornGraphView.cs
+++ Scripts/Editor/CapricornGraphView.cs
@@ -2,7 +2,9 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
+
 using Newtonsoft.Json;
 
 namespace Dunward
@@ -19,6 +21,7 @@ namespace Dunward
             this.AddManipulator(new ContentDragger());
             this.AddManipulator(new SelectionDragger());
             this.AddManipulator(new RectangleSelector());
+            this.AddManipulator(new DragAndDropManipulator());
 
             this.AddManipulator(new ContextualMenuManipulator(evt => evt.menu.AppendAction("Add Node", action => AddNode(action.eventInfo.localMousePosition), DropdownMenuAction.AlwaysEnabled)));
         }
@@ -57,6 +60,58 @@ namespace Dunward
             var node = new CapricornGraphNode(this, lastNodeID, position);
             AddElement(node);
             lastNodeID++;
+        }
+
+        private class DragAndDropManipulator : PointerManipulator
+        {
+            private Object item;
+
+            public DragAndDropManipulator()
+            {
+                
+            }
+
+            protected override void RegisterCallbacksOnTarget()
+            {
+                target.RegisterCallback<DragEnterEvent>(OnDragEnter);
+                target.RegisterCallback<DragLeaveEvent>(OnDragLeave);
+                target.RegisterCallback<DragUpdatedEvent>(OnDragUpdated);
+                target.RegisterCallback<DragPerformEvent>(OnDragPerform);
+            }
+
+            protected override void UnregisterCallbacksFromTarget()
+            {
+                target.UnregisterCallback<DragEnterEvent>(OnDragEnter);
+                target.UnregisterCallback<DragLeaveEvent>(OnDragLeave);
+                target.UnregisterCallback<DragUpdatedEvent>(OnDragUpdated);
+                target.UnregisterCallback<DragPerformEvent>(OnDragPerform);
+            }
+
+            private void OnDragEnter(DragEnterEvent _)
+            {
+                item = DragAndDrop.objectReferences[0];
+            }
+
+            private void OnDragLeave(DragLeaveEvent _)
+            {
+                item = null;
+            }
+
+            private void OnDragUpdated(DragUpdatedEvent _)
+            {
+                DragAndDrop.visualMode = IsSingleTextAsset() ? DragAndDropVisualMode.Link : DragAndDropVisualMode.Rejected;
+            }
+
+            private void OnDragPerform(DragPerformEvent evt)
+            {
+                var textAsset = item as TextAsset;
+                Debug.LogError(textAsset.text);
+            }
+
+            private bool IsSingleTextAsset()
+            {
+                return item is TextAsset && DragAndDrop.objectReferences.Length == 1;
+            }
         }
     }
 }
