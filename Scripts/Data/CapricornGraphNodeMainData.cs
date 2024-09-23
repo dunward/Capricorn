8b7ed19cf1a7ca01b0f2350753229f98bc86bea0
Elik Song <elik.song@icloud.com>
2024-09-23 21:25:45 +0900

    Add position in main data

--- Scripts/Data/CapricornGraphNodeMainData.cs
+++ Scripts/Data/CapricornGraphNodeMainData.cs
@@ -4,6 +4,8 @@ namespace Dunward
     public class CapricornGraphNodeMainData
     {
         public int id;
+        public float x;
+        public float y;
         public CapricornGraphNodeActionData actionData;
     }
 }
