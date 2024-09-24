ca6b61973b460fedd4ab98149ce1824ef183425f
Elik Song <elik.song@icloud.com>
2024-09-24 21:10:19 +0900

    Edit header colors

--- Scripts/Utils/CapricornColors.cs
+++ Scripts/Utils/CapricornColors.cs
@@ -5,8 +5,8 @@ namespace Dunward.Capricorn
 {
     public static class CapricornColors
     {
-        public static Color InputTitleHeader => Color.green;
-        public static Color OutputTitleHeader => Color.red;
+        public static Color InputTitleHeader => new Color(0.2f, 0.47f, 0.16f);
+        public static Color OutputTitleHeader => new Color(0.59f, 0.19f, 0.19f);
         public static Color Port => new Color(0.69f, 0.98f, 0.34f);
     }
 }
