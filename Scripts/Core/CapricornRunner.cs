fa9269401783887d01da35c25b0dee172f02ed3a
Min Song <min.song@bagelcode.com>
2024-09-25 18:10:17 +0900

    Add runner mono on construct

--- Scripts/Core/CapricornRunner.cs
+++ Scripts/Core/CapricornRunner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using UnityEngine;
 using UnityEngine.UI;
 
 using Newtonsoft.Json;
@@ -11,6 +12,8 @@ namespace Dunward.Capricorn
     public class CapricornRunner
     {
         private GraphData graphData;
+        
+        private MonoBehaviour target;
 
 #region Test
         public TextMeshProUGUI nameTmp;
@@ -23,8 +26,10 @@ namespace Dunward.Capricorn
         public readonly NodeMainData startNode;
         private Dictionary<int, NodeMainData> nodes = new Dictionary<int, NodeMainData>();
 
-        public CapricornRunner(string text)
+        public CapricornRunner(string text, MonoBehaviour target)
         {
+            this.target = target;
+
             graphData = JsonConvert.DeserializeObject<GraphData>(text);
             startNode = graphData.nodes.Find(node => node.nodeType == NodeType.Input);
 
