5bc54c11696ee7baab74e83eefb8f1d07e035996
Min Song <min.song@bagelcode.com>
2024-09-25 18:40:38 +0900

    Add wait coroutine unit

--- Scripts/Unit/CoroutineUnit.cs
+++ Scripts/Unit/CoroutineUnit.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEditor;
 
 namespace Dunward.Capricorn
 {
@@ -10,17 +9,22 @@ namespace Dunward.Capricorn
 #if UNITY_EDITOR
         protected virtual string info => "Coroutine Element";
 
-        public virtual void OnGUI(Rect rect, int index, bool isActive, bool isFocused)
+        public void OnGUI(Rect rect, int index, bool isActive, bool isFocused)
         {
-            rect.y -= 10;
-            EditorGUI.LabelField(rect, info);
-            rect.y += EditorGUIUtility.singleLineHeight;
-            isWaitingUntilFinish = EditorGUI.ToggleLeft(rect, "Waiting Until Finish", isWaitingUntilFinish);
+            var singleHeight = UnityEditor.EditorGUIUtility.singleLineHeight;
+            var height = 0f;
+            UnityEditor.EditorGUI.LabelField(new Rect(rect.x, rect.y, rect.width, singleHeight), info);
+            height += singleHeight;
+            isWaitingUntilFinish = UnityEditor.EditorGUI.Toggle(new Rect(rect.x, rect.y + height, rect.width, singleHeight), "Waiting Until Finish", isWaitingUntilFinish);
+            height += singleHeight;
+            OnGUI(rect, height);
         }
+
+        public abstract void OnGUI(Rect rect, float height);
 
         public virtual float GetHeight()
         {
-            return EditorGUIUtility.singleLineHeight * 2;
+            return UnityEditor.EditorGUIUtility.singleLineHeight * 2;
         }
 #endif
     }
---
+++ Scripts/Unit/WaitUnit.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Dunward.Capricorn
+{
+    [System.Serializable]
+    public class WaitUnit : CoroutineUnit
+    {
+        public float time;
+
+#if UNITY_EDITOR
+        protected override string info => $"Wait {time} seconds";
+
+        public override void OnGUI(Rect rect, float height)
+        {
+            var singleHeight = UnityEditor.EditorGUIUtility.singleLineHeight;
+            time = Mathf.Clamp(UnityEditor.EditorGUI.FloatField(new Rect(rect.x, rect.y + height, rect.width, singleHeight), "Time", time), 0, float.MaxValue);
+        }
+
+        public override float GetHeight()
+        {
+            return base.GetHeight() + UnityEditor.EditorGUIUtility.singleLineHeight;
+        }
+#endif
+    }
+}
