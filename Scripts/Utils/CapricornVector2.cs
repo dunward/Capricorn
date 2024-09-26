b510dec08fa9fd87031c845a8494634b9ffe2d6a
Elik Song <elik.song@icloud.com>
2024-09-26 22:10:11 +0900

    Add serialize vector2 class

---
+++ Scripts/Utils/CapricornVector2.cs
@@ -0,0 +1,28 @@
+namespace Dunward.Capricorn
+{
+    [System.Serializable]
+    public struct CapricornVector2
+    {
+        public float x;
+        public float y;
+
+        public static implicit operator CapricornVector2(UnityEngine.Vector2 vector2)
+        {
+            return new CapricornVector2
+            {
+                x = vector2.x,
+                y = vector2.y
+            };
+        }
+
+        public static implicit operator UnityEngine.Vector2(CapricornVector2 vector2)
+        {
+            return new UnityEngine.Vector2(vector2.x, vector2.y);
+        }
+
+        public static implicit operator UnityEngine.Vector3(CapricornVector2 vector2)
+        {
+            return new UnityEngine.Vector3(vector2.x, vector2.y, 0);
+        }
+    }
+}
