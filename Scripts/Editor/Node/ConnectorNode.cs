d0872fe1e5c7dabd293d8b877e857f0901f3c0ea
Elik Song <elik.song@icloud.com>
2024-10-01 12:33:49 +0900

    Add connector to output changer

--- Scripts/Editor/Node/ConnectorNode.cs
+++ Scripts/Editor/Node/ConnectorNode.cs
@@ -1,12 +1,15 @@
 #if UNITY_EDITOR
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEditor.Experimental.GraphView;
 
 namespace Dunward.Capricorn
 {
     public class ConnectorNode : BaseNode
     {
         private TextField customField;
+        private ContextualMenuManipulator convertToOutputMenuManipulator;
+        private ContextualMenuManipulator convertToConnectorMenuManipulator;
 
         public ConnectorNode(GraphView graphView, int id, float x, float y) : base(graphView, id, x, y)
         {
@@ -26,6 +29,16 @@ namespace Dunward.Capricorn
         protected override void Initialize()
         {
             nodeType = NodeType.Connector;
+
+            convertToOutputMenuManipulator = new ContextualMenuManipulator(evt => evt.menu.AppendAction("Convert to Output Node",
+                        (action) => ConvertToOutputNode(),
+                        DropdownMenuAction.AlwaysEnabled));
+
+            convertToConnectorMenuManipulator = new ContextualMenuManipulator(evt => evt.menu.AppendAction("Convert to Connector Node",
+                        (action) => ConvertToConnectorNode(),
+                        DropdownMenuAction.AlwaysEnabled));
+
+            this.AddManipulator(convertToOutputMenuManipulator);
         }
 
         protected override void SetupTitleContainer()
@@ -50,6 +63,33 @@ namespace Dunward.Capricorn
         protected override void Repaint()
         {
             customField.value = string.IsNullOrEmpty(customTitle) ? $"{id}" : customTitle;
+        }
+
+        private void ConvertToOutputNode()
+        {
+            nodeType = NodeType.Output;
+            var topHeader = new VisualElement();
+            topHeader.AddToClassList("capricorn-title-container-output");
+            titleContainer.Add(topHeader);
+            title = "Output";
+            customTitle = string.Empty;
+
+            titleContainer.Remove(customField);
+            this.AddManipulator(convertToConnectorMenuManipulator);
+            this.RemoveManipulator(convertToOutputMenuManipulator);
+        }
+
+        private void ConvertToConnectorNode()
+        {
+            nodeType = NodeType.Connector;
+            SetupTitleContainer();
+
+            // get capricorn-title-container-output class and remove it
+            titleContainer.RemoveAt(titleContainer.childCount - 1);
+            title = string.Empty;
+
+            this.AddManipulator(convertToOutputMenuManipulator);
+            this.RemoveManipulator(convertToConnectorMenuManipulator);
         }
     }
 }
