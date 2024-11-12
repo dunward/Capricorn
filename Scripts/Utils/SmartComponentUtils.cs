ce1ce5b448a415804b747fcb3659cd55a1c05bca
Elik Song <elik.song@icloud.com>
2024-11-12 21:33:32 +0900

    Remove unused utils

--- Scripts/Utils/SmartComponentUtils.cs
+++ Scripts/Utils/SmartComponentUtils.cs
@@ -22,37 +22,5 @@ namespace Dunward.Capricorn
                     throw new System.Exception("TextUtils.SetText() is not implemented for this type.");
             }
         }
-
-        public static void AppendText(this object textComponent, char text)
-        {
-            switch (textComponent)
-            {
-                case TMP_Text tmpText:
-                    tmpText.text += text;
-                    break;
-                case Text uiText:
-                    uiText.text += text;
-                    break;
-                case null:
-                    break;
-                default:
-                    throw new System.Exception("TextUtils.AppendText() is not implemented for this type.");
-            }
-        }
-
-        public static string GetText(this object textComponent)
-        {
-            switch (textComponent)
-            {
-                case TMP_Text tmpText:
-                    return tmpText.text;
-                case Text uiText:
-                    return uiText.text;
-                case null:
-                    return null;
-                default:
-                    throw new System.Exception("TextUtils.GetText() is not implemented for this type.");
-            }
-        }
     }
 }
